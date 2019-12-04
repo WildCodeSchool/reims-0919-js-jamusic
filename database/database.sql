e174c451402b114cdc91153061df006c46ae3c97
Jessy Coraboeuf <coraboeufjessy@gmail.com>
2019-12-04 16:40:36 +0100

    added database.sql script file for the creation of the database

---
+++ database/database.sql
@@ -0,0 +1,44 @@
+DROP DATABASE IF EXISTS jamusic;
+CREATE DATABASE jamusic;
+USE jamusic;
+DROP TABLE IF EXISTS profile;
+CREATE TABLE profile
+(
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    nickname VARCHAR(32) NOT NULL,
+    firstname VARCHAR(50) NOT NULL,
+    lastname VARCHAR(50) NOT NULL
+);
+DROP TABLE IF EXISTS tag;
+CREATE TABLE tag(
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    instrument VARCHAR(50) NOT NULL,
+    status VARCHAR(15) NOT NULL,
+    style VARCHAR(50) NOT NULL
+);
+DROP TABLE IF EXISTS post;
+CREATE TABLE post(
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    text VARCHAR(400) NOT NULL,
+    note INT NULL,
+    share INT NOT NULL,
+    profile_id INT,
+    FOREIGN KEY (profile_id) REFERENCES profile(id)
+);
+DROP TABLE IF EXISTS message;
+CREATE TABLE message
+(
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    text VARCHAR(1000) NOT NULL,
+    profile_id INT,
+    FOREIGN KEY (profile_id) REFERENCES profile(id)
+);
+DROP TABLE IF EXISTS account;
+CREATE TABLE account
+(
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    email VARCHAR(320) NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    profile_id INT,
+    FOREIGN KEY (profile_id) REFERENCES profile(id)
+);
