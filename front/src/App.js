d727bf34d0e99b53d4ddff6e356c48f9489253ce
Brolyus <desousanthony@gmail.com>
2019-12-05 18:00:12 +0100

    added axios and suceed to call API on app.js

--- front/src/App.js
+++ front/src/App.js
@@ -1,11 +1,36 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      profiles: [],
+      tags: []
+    };
+  }
+
+  componentDidMount() { 
+    // axios.get(`http://localhost:3000/profiles`)
+    // .then(profilesRes => {
+    //   const profiles = profilesRes.data;
+    //   this.setState({ profiles });
+    // })
+    axios.all([
+      axios.get('http://localhost:3000/profiles'),
+      axios.get('http://localhost:3000/tags')
+    ])
+    .then(axios.spread((profilesRes, tagsRes) => {
+      const profiles = profilesRes.data
+      const tags = tagsRes.data
+      this.setState({profiles, tags})
+    }))
+}
+
+  render() {
+    return <div className="App"><h1>Welcome to jaMusic</h1></div>;
+  }
 }
 
 export default App;
