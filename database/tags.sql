b33e24ebfefd153e8be2849a38dfdf1a2091667a
Jessy Coraboeuf <coraboeufjessy@gmail.com>
2019-12-05 14:33:00 +0100

    Added the tag.sql file to insert test data into the tag table

---
+++ database/tags.sql
@@ -0,0 +1,7 @@
+USE jamusic;
+INSERT INTO tag (instrument, status, style)
+VALUES ("Triangle","Pro","Classique"),
+("Saxophone","Pro","Dubstep"),
+("Basson","Amateur","Rock'n'Roll"),
+("Contrebasse","Pro","Jazz"),
+("Cor d'harmonie","Amateur","Orchestre");
