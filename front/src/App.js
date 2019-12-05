e21290bf7d0b430dd36689550413c52e7875e383
Pierre-Alban DEMARS <demarspierrealban@gmail.com>
2019-12-05 17:30:32 +0100

    Add Tags component with tests before using API

--- front/src/App.js
+++ front/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Profiles from './Components/Profiles'
+import Tags from './Components/Tags'
 
 function App() {
   return (
     <div className="App">
       <Profiles />
+      <Tags />
     </div>
   );
 }
