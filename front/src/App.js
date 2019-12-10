cafd9f57185a923ac25174a7bc380d9dc8170e7d
Jessy Coraboeuf <coraboeufjessy@gmail.com>
2019-12-10 15:37:21 +0100

    Added a basic input form for the email, password and nickname

--- front/src/App.js
+++ front/src/App.js
@@ -1,44 +1,42 @@
-import React from "react";
-import "./App.css";
-import axios from "axios";
-import Tags from "./Components/Tags";
-import Profiles from "./Components/Profiles";
+import React from "react"
+import "./App.css"
+import axios from "axios"
+import CreateProfile from "./Components/CreateProfile"
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      profiles: [],
-      tags: []
-    };
-  }
+    constructor(props) {
+        super(props)
+        this.state = {
+            profiles: [],
+            tags: []
+        }
+    }
 
-  componentDidMount() {
-    axios
-      .all([
-        axios.get("http://localhost:3000/profiles"),
-        axios.get("http://localhost:3000/tags")
-      ])
-      .then(
-        axios.spread((profilesRes, tagsRes) => {
-          const profiles = profilesRes.data;
-          const tags = tagsRes.data;
-          this.setState({ profiles, tags });
-        })
-      );
-  }
+    componentDidMount() {
+        axios
+            .all([
+                axios.get("http://localhost:3000/profiles"),
+                axios.get("http://localhost:3000/tags")
+            ])
+            .then(
+                axios.spread((profilesRes, tagsRes) => {
+                    const profiles = profilesRes.data
+                    const tags = tagsRes.data
+                    this.setState({ profiles, tags })
+                })
+            )
+    }
 
-  render() {
-    return (
-      <div className="App">
-        <h1>Welcome to jaMusic</h1>
-        <div className='rendering'>
-          <Profiles profiles={this.state.profiles} />
-          <Tags tags={this.state.tags} />
-        </div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <h1>Welcome to jaMusic</h1>
+                <div className="rendering">
+                    <CreateProfile />
+                </div>
+            </div>
+        )
+    }
 }
 
-export default App;
+export default App
