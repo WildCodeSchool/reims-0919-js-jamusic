e327716b8fad862af6fe0d087804f045c65abf55
Jessy Coraboeuf <coraboeufjessy@gmail.com>
2019-12-10 15:37:21 +0100

    Added a basic input form for the email, password and nickname

--- front/src/Components/CreateProfile.jsx
+++ front/src/Components/CreateProfile.jsx
@@ -1,7 +1,23 @@
 import React from "react"
 
 const CreateProfile = () => {
-    return <div>Test</div>
+    return (
+        <form action="">
+            <label htmlFor="email">Email :</label>
+            <br />
+            <input type="email" name="email" id="email" />
+            <br />
+            <label htmlFor="password">Mot de passe :</label>
+            <br />
+            <input type="password" name="password" id="password" />
+            <br />
+            <label htmlFor="nickname">Pseudonyme :</label>
+            <br />
+            <input type="text" name="nickname" id="nickname" />
+            <br />
+            <button type="submit">S'inscrire</button>
+        </form>
+    )
 }
 
 export default CreateProfile
