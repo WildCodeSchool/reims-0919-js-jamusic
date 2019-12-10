bac6a37169f32f38c7c5065fa881fa5886df05d3
Jessy Coraboeuf <coraboeufjessy@gmail.com>
2019-12-10 15:22:48 +0100

    fixing a stupid mistake

--- front/src/Components/CreateProfile.jsx
+++ front/src/Components/CreateProfile.jsx
@@ -1,10 +1,7 @@
-import React from 'react'
+import React from "react"
 
 const CreateProfile = () => {
-
-    return{
-        <div>Test</div>
-    }
+    return <div>Test</div>
 }
 
 export default CreateProfile
