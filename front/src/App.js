bc5c670ce7cae270202de0c98f61f371c5625f2c
Brolyus <desousanthony@gmail.com>
2019-12-12 18:00:24 +0100

    added a button to show/ hide the menu

--- front/src/App.js
+++ front/src/App.js
@@ -11,10 +11,11 @@ class App extends React.Component {
     this.state = {
       profiles: [],
       tags: [],
-      researchIsVisible: true,
+      researchIsVisible: false,
       selectedTags: []
     };
-    this.handleSelectedTags = this.handleSelectedTags.bind(this)
+    this.handleSelectedTags = this.handleSelectedTags.bind(this);
+    this.handleresearchIsVisible = this.handleresearchIsVisible.bind(this);
   }
 
   componentDidMount() {
@@ -31,19 +32,45 @@ class App extends React.Component {
         })
       );
   }
-  handleSelectedTags (instrument) {
-    this.state.selectedTags.includes(instrument) ?
-    this.setState({selectedTags: this.state.selectedTags.filter(tag => tag !== instrument)}) :
-    this.setState({selectedTags: [...this.state.selectedTags, instrument]})
+
+  handleresearchIsVisible() {
+    this.setState({ researchIsVisible: !this.state.researchIsVisible });
+  }
+
+  handleSelectedTags(instrument) {
+    this.state.selectedTags.includes(instrument)
+      ? this.setState({
+          selectedTags: this.state.selectedTags.filter(
+            tag => tag !== instrument
+          )
+        })
+      : this.setState({
+          selectedTags: [...this.state.selectedTags, instrument]
+        });
   }
   render() {
     return (
       <div className="App">
-          <h1>Welcome to jaMusic</h1>
-          <Profiles profiles={this.state.profiles} />
-          <Tags tags={this.state.tags} />
-          {this.state.researchIsVisible && 
-          <Search tags={this.state.tags} handleSelectedTags={this.handleSelectedTags} selectedTags={this.state.selectedTags}/>}
+        {this.state.researchIsVisible ? null : (
+          <img
+            src="https://img.icons8.com/metro/26/000000/chevron-left.png"
+            className="menu_icon"
+            alt="chevron"
+            onClick={this.handleresearchIsVisible}
+          />
+        )}
+        <h1>Welcome to jaMusic</h1>
+        <Profiles profiles={this.state.profiles} />
+        <Tags tags={this.state.tags} />
+        {this.state.researchIsVisible && (
+          <Search
+            tags={this.state.tags}
+            handleSelectedTags={this.handleSelectedTags}
+            selectedTags={this.state.selectedTags}
+            researchIsVisible={this.state.researchIsVisible}
+            handleresearchIsVisible={this.handleresearchIsVisible}
+          />
+        )}
       </div>
     );
   }
--- front/src/Components/Search.jsx
+++ front/src/Components/Search.jsx
@@ -1,21 +1,29 @@
 import React from "react";
-import "./Search.css"
+import "./Search.css";
 
-const Search = (props) => {
-
+const Search = props => {
   return (
     <div className="search_sidebar">
+      {props.researchIsVisible && <img src="https://img.icons8.com/material-outlined/24/000000/chevron-left.png"  alt="chevron"
+            onClick={props.handleresearchIsVisible}></img>}
       <p>Selectionnez vos tags:</p>
       <ul>
-      {props.tags.map(tag => {
-        return(
-          <li key={tag.id} onClick={() => props.handleSelectedTags(tag.instrument)} className={props.selectedTags.includes(tag.instrument) ? "is_selected" : "not_selected"}>{`#${tag.instrument}`}</li>
-        )})}
-      
+        {props.tags.map(tag => {
+          return (
+            <li
+              key={tag.id}
+              onClick={() => props.handleSelectedTags(tag.instrument)}
+              className={
+                props.selectedTags.includes(tag.instrument)
+                  ? "is_selected"
+                  : "not_selected"
+              }
+            >{`#${tag.instrument}`}</li>
+          );
+        })}
       </ul>
-
     </div>
-  )
-}
+  );
+};
 
 export default Search;
