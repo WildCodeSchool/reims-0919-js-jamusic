52718bf612b3a9cf1e06af7c8535539e8581c769
Pierre-Alban DEMARS <demarspierrealban@gmail.com>
2019-12-12 15:58:03 +0100

    Standardization of code with prettier

--- back/index.js
+++ back/index.js
@@ -1,97 +1,112 @@
-const express = require("express");
-const app = express();
-const port = 3000;
-const connection = require("./config");
-const bodyParser = require("body-parser");
+const express = require('express')
+const app = express()
+const port = 3000
+const connection = require('./config')
+const bodyParser = require('body-parser')
 const cors = require('cors')
 
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 app.use(cors())
 
-app.get("/", (request, response) => {
-  response.send("Welcome to jaMusic Server");
-});
+app.get('/', (request, response) => {
+    response.send('Welcome to jaMusic Server')
+})
 
-app
-  .route("/profiles")
+app.route('/profiles')
 
-  .get((request, response) => {
-    connection.query("SELECT * from profile", (err, results) => {
-      if (err) {
-        response.status(500).send("Error retrieving profiles");
-      } else {
-        response.json(results);
-      }
-    });
-  })
-
-  .post((request, response) => {
-    const formData = request.body;
-    connection.query("INSERT INTO profile SET ?;", formData, (err, results) => {
-      if (err) {
-        console.log(err);
-        res.status(500).send("Error adding a new profile");
-      } else {
-        response.json(results);
-      }
-    });
-  });
-
-app
-  .route("/profiles/:id")
-  .get((request,response) => {
-    const idProfile = request.params.id;
-    connection.query('SELECT * FROM profile WHERE id = ?', [idProfile], (err,results) => {
-      if (err) {
-        console.log(err);
-        response.status(500).send("Erreur dans la récupération du profile")
-      } else {
-        response.json(results);
-      }
+    .get((request, response) => {
+        connection.query('SELECT * from profile', (err, results) => {
+            if (err) {
+                response.status(500).send('Error retrieving profiles')
+            } else {
+                response.json(results)
+            }
+        })
     })
-  })  
 
-  .put((request,response) => {
-    const idProfile = request.params.id;
-    const formData = request.body;
-    connection.query('UPDATE profile SET ? WHERE id = ?', [formData, idProfile], err => {
-      if (err) {
-        console.log(err);
-        response.status(500).send("Error editing a profile");
-      } else {
-        response.sendStatus(200);
-      }
+    .post((request, response) => {
+        const formData = request.body
+        connection.query(
+            'INSERT INTO profile SET ?;',
+            formData,
+            (err, results) => {
+                if (err) {
+                    console.log(err)
+                    res.status(500).send('Error adding a new profile')
+                } else {
+                    response.json(results)
+                }
+            }
+        )
     })
-  })
 
-app
-  .route("/tags")
+app.route('/profiles/:id')
+    .get((request, response) => {
+        const idProfile = request.params.id
+        connection.query(
+            'SELECT * FROM profile WHERE id = ?',
+            [idProfile],
+            (err, results) => {
+                if (err) {
+                    console.log(err)
+                    response
+                        .status(500)
+                        .send('Erreur dans la récupération du profile')
+                } else {
+                    response.json(results)
+                }
+            }
+        )
+    })
 
-  .get((request, response) => {
-    connection.query("SELECT * from tag", (err, results) => {
-      if (err) {
-        response.status(500).send("Error retrieving tags");
-      } else {
-        response.json(results);
-      }
-    });
-  })
-  .post((request, response) => {
-    const formData = request.body;
-    connection.query("INSERT INTO profile SET ?;", formData, (err, results) => {
-      if (err) {
-        console.log(err);
-        res.status(500).send("Error adding a new profile");
-      } else {
-        response.json(results);
-      }
-    });
-  });
+    .put((request, response) => {
+        const idProfile = request.params.id
+        const formData = request.body
+        connection.query(
+            'UPDATE profile SET ? WHERE id = ?',
+            [formData, idProfile],
+            err => {
+                if (err) {
+                    console.log(err)
+                    response.status(500).send('Error editing a profile')
+                } else {
+                    response.sendStatus(200)
+                }
+            }
+        )
+    })
+
+app.route('/tags')
+
+    .get((request, response) => {
+        connection.query('SELECT * from tag', (err, results) => {
+            if (err) {
+                response.status(500).send('Error retrieving tags')
+            } else {
+                response.json(results)
+            }
+        })
+    })
+    .post((request, response) => {
+        const formData = request.body
+        connection.query(
+            'INSERT INTO profile SET ?;',
+            formData,
+            (err, results) => {
+                if (err) {
+                    console.log(err)
+                    res.status(500).send('Error adding a new profile')
+                } else {
+                    response.json(results)
+                }
+            }
+        )
+    })
 
 app.listen(port, err => {
-  if (err) {
-    throw new Error("Something bad happened...");
-  }
+    if (err) {
+        throw new Error('Something bad happened...')
+    }
 
-  console.log(`Server is listening on ${port}`);
-});
+    console.log(`Server is listening on ${port}`)
+})
--- front/src/App.js
+++ front/src/App.js
@@ -1,40 +1,40 @@
-import React from "react";
-import "./App.css";
-import axios from "axios";
-import Profile from "./Components/Profile";
+import React from 'react'
+import './App.css'
+import axios from 'axios'
+import Profile from './Components/Profile'
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      profiles: [],
-      tags: []
-    };
-  }
+    constructor(props) {
+        super(props)
+        this.state = {
+            profiles: [],
+            tags: []
+        }
+    }
 
-  componentDidMount() {
-    axios
-      .all([
-        axios.get("http://localhost:3000/profiles"),
-        axios.get("http://localhost:3000/tags")
-      ])
-      .then(
-        axios.spread((profilesRes, tagsRes) => {
-          const profiles = profilesRes.data;
-          const tags = tagsRes.data;
-          this.setState({ profiles, tags });
-        })
-      );
-  }
+    componentDidMount() {
+        axios
+            .all([
+                axios.get('http://localhost:3000/profiles'),
+                axios.get('http://localhost:3000/tags')
+            ])
+            .then(
+                axios.spread((profilesRes, tagsRes) => {
+                    const profiles = profilesRes.data
+                    const tags = tagsRes.data
+                    this.setState({ profiles, tags })
+                })
+            )
+    }
 
-  render() {
-    return (
-      <div className='app'>
-        <h1 className='title'>JaMusic</h1>
-          <Profile />
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className='app'>
+                <h1 className='title'>JaMusic</h1>
+                <Profile />
+            </div>
+        )
+    }
 }
 
-export default App;
+export default App
