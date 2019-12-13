873dbfe46719ec4bf3a694705b90d65f20494cc1
Jessy Coraboeuf <coraboeufjessy@gmail.com>
2019-12-13 09:48:36 +0100

    Deleted a file that shouldn't have been here in the first place.

--- back/config.js
+++
@@ -1,8 +0,0 @@
-const  mysql = require('mysql');
-const  connection = mysql.createConnection({
-host :  'localhost', // address of the server
-user :  'admin', // username
-password :  'Jecode4wcs!',
-database :  'jamusic',
-});
-module.exports = connection;
