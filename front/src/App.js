0a9c33b771c808cb4f168464bd2db9afd1a20032
Pierre-Alban DEMARS <demarspierrealban@gmail.com>
2019-12-13 12:01:48 +0100

    Profilecomponent uses database

--- front/src/App.js
+++ front/src/App.js
@@ -1,42 +1,49 @@
-
-import React from "react";
-import "./App.css";
-import axios from "axios";
-import Profile from "./Components/Profile";
+import React from 'react'
+import './App.css'
+import axios from 'axios'
+import Profile from './Components/Profile'
 
 class App extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
             profiles: [],
-            tags: []
+            tags: [],
+            isLoaded: false
         }
     }
 
     componentDidMount() {
         axios
             .all([
-                axios.get('http://localhost:3000/profiles'),
+                axios.get('http://localhost:3000/profiles/2'),
                 axios.get('http://localhost:3000/tags')
             ])
             .then(
                 axios.spread((profilesRes, tagsRes) => {
                     const profiles = profilesRes.data
                     const tags = tagsRes.data
-                    this.setState({ profiles, tags })
+                    this.setState({
+                        profiles,
+                        tags,
+                        isLoaded: true
+                    })
                 })
             )
     }
 
-
-  render() {
-    return (
-      <div className='app'>
-        <h1 className='title'>JaMusic</h1>
-          <Profile />
-      </div>
-    );
-  }
+    render() {
+        if (!this.state.isLoaded) {
+            return <h2>Loading ...</h2>
+        } else {
+            return (
+                <div className='app'>
+                    <h1 className='title'>JaMusic</h1>
+                    <Profile profile={this.state.profiles} />
+                </div>
+            )
+        }
+    }
 }
 
 export default App
