9b8d204bfb6a3f53c609156b4abb1f28907c6bcd
Pierre-Alban DEMARS <demarspierrealban@gmail.com>
2019-12-16 11:37:29 +0100

    Component created, ready to export

---
+++ front/src/Components/LoginForm.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class LoginForm extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            email: '',
+            password: ''
+        }
+    }
+}
+
+export default LoginForm
