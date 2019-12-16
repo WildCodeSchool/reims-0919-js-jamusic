9bda789a0e31350093dc9e41ed5e9aa1b7669cab
Jessy Coraboeuf <coraboeufjessy@gmail.com>
2019-12-16 10:08:56 +0100

    Removed the line break tags and the nickname field, aligned the fields with css instead

--- front/src/Components/CreateProfile.jsx
+++ front/src/Components/CreateProfile.jsx
@@ -4,22 +4,12 @@ import './CreateProfile.css'
 const CreateProfile = () => {
     return (
         <form method='post' className='createProfileForm'>
-            <label htmlFor='nickname'>Pseudonyme :</label>
-            <br />
-            <input type='text' name='nickname' id='nickname' required />
-            <br />
             <label htmlFor='email'>Adresse mail :</label>
-            <br />
             <input type='email' name='email' id='email' required />
-            <br />
             <label htmlFor='password'>Mot de passe :</label>
-            <br />
             <input type='password' name='password' id='password' required />
-            <br />
             <label htmlFor='password2'>Confirmation du mot de passe :</label>
-            <br />
             <input type='password' name='password2' id='password2' />
-            <br />
             <button type='submit' id='submit'>
                 S'inscrire
             </button>
