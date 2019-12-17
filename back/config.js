eb5fb8231b9e99c87ed6ff4ee43df5af2e5e3b8b
Brolyus <desousanthony@gmail.com>
2019-12-17 17:52:12 +0100

    token part work

--- back/config.js
+++ back/config.js
@@ -1,8 +1,8 @@
-const  mysql = require('mysql');
-const  connection = mysql.createConnection({
-host :  'localhost', // address of the server
-user :  'admin', // username
-password :  'Jecode4wcs!',
-database :  'jamusic',
-});
-module.exports = connection;
+const mysql = require('mysql')
+const connection = mysql.createConnection({
+    host: 'localhost', // address of the server
+    user: 'admin', // username
+    password: 'Jecode4wcs!',
+    database: 'jamusic'
+})
+module.exports = connection
