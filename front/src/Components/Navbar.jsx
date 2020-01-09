528dde55d1ab14674a52100d98f02c79fdc83ddf
Pierre-Alban DEMARS <demarspierrealban@gmail.com>
2020-01-09 12:31:41 +0100

    CSS of link in Navbar.jsx

--- front/src/Components/Navbar.jsx
+++ front/src/Components/Navbar.jsx
@@ -15,16 +15,16 @@ library.add(faCommentDots, faTags, faUser, faScroll)
 const Navbar = () => {
     return (
         <div className='Navbar'>
-            <Link to=''>
+            <Link to='' className='navlink'>
                 <FontAwesomeIcon icon='scroll' className='navIcon' />
             </Link>
-            <Link to='/tags'>
+            <Link to='/tags' className='navlink'>
                 <FontAwesomeIcon icon='tags' className='navIcon' />
             </Link>
-            <Link to='/profiles'>
+            <Link to='/profiles' className='navlink'>
                 <FontAwesomeIcon icon='user' className='navIcon' />
             </Link>
-            <Link to=''>
+            <Link to='' className='navlink'>
                 <FontAwesomeIcon icon='comment-dots' className='navIcon' />
             </Link>
         </div>
