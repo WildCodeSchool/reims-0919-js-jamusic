edef7fe729d81e3ef101b6cb041d7b21816d2600
Pierre-Alban DEMARS <demarspierrealban@gmail.com>
2020-01-09 09:36:54 +0100

    ADD Replace Navbar

--- front/src/Components/Navbar.jsx
+++ front/src/Components/Navbar.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import {
-  faCommentDots,
-  faTags,
-  faUser,
-  faScroll
+    faCommentDots,
+    faTags,
+    faUser,
+    faScroll
 } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import './Navbar.css'
@@ -12,14 +12,14 @@ import './Navbar.css'
 library.add(faCommentDots, faTags, faUser, faScroll)
 
 const Navbar = () => {
-  return (
-    <div className='Navbar'>
-      <FontAwesomeIcon icon='scroll' className='navIcon' />
-      <FontAwesomeIcon icon='tags' className='navIcon' />
-      <FontAwesomeIcon icon='user' className='navIcon' />
-      <FontAwesomeIcon icon='comment-dots' className='navIcon' />
-    </div>
-  )
+    return (
+        <div className='Navbar'>
+            <FontAwesomeIcon icon='scroll' className='navIcon' />
+            <FontAwesomeIcon icon='tags' className='navIcon' />
+            <FontAwesomeIcon icon='user' className='navIcon' />
+            <FontAwesomeIcon icon='comment-dots' className='navIcon' />
+        </div>
+    )
 }
 
 export default Navbar
