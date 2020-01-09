98cd3e6b5dd76f4c27ac0075d731b773861fbdf6
Pierre-Alban DEMARS <demarspierrealban@gmail.com>
2020-01-09 10:28:46 +0100

    ADD Link in Navbar and delete line css to correct problem

--- front/src/Components/Navbar.jsx
+++ front/src/Components/Navbar.jsx
@@ -8,16 +8,25 @@ import {
 } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import './Navbar.css'
+import { Link } from 'react-router-dom'
 
 library.add(faCommentDots, faTags, faUser, faScroll)
 
 const Navbar = () => {
     return (
         <div className='Navbar'>
-            <FontAwesomeIcon icon='scroll' className='navIcon' />
-            <FontAwesomeIcon icon='tags' className='navIcon' />
-            <FontAwesomeIcon icon='user' className='navIcon' />
-            <FontAwesomeIcon icon='comment-dots' className='navIcon' />
+            <Link to=''>
+                <FontAwesomeIcon icon='scroll' className='navIcon' />
+            </Link>
+            <Link to='/tags'>
+                <FontAwesomeIcon icon='tags' className='navIcon' />
+            </Link>
+            <Link to='/profiles'>
+                <FontAwesomeIcon icon='user' className='navIcon' />
+            </Link>
+            <Link to=''>
+                <FontAwesomeIcon icon='comment-dots' className='navIcon' />
+            </Link>
         </div>
     )
 }
