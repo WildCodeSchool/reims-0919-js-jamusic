f16e527a5bd424b2be23d49a0156d3859ebda73f
Brolyus <desousanthony@gmail.com>
2020-01-09 14:43:32 +0100

    function authenticateToken is written, will be used later to grant access to pages only if token is valid

--- back/index.js
+++ back/index.js
@@ -11,6 +11,24 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
 
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+    if (token == null) {
+        return res.sendStatus(401)
+    } else {
+        jwt.verify(token, secret, (err, user) => {
+            console.log(err)
+            if (err) {
+                return res.sendStatus(403)
+            } else {
+                req.user = user
+            }
+            next()
+        })
+    }
+}
+
 app.get('/', (request, response) => {
     response.send('Welcome to jaMusic Server')
 })
