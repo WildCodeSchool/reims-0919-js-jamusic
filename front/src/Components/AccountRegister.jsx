eb4b100e74457f39996a43524812a608bcd39263
Pierre-Alban DEMARS <demarspierrealban@gmail.com>
2020-01-09 09:39:44 +0100

    Indent problems after hackathon here's the patch

--- front/src/Components/AccountRegister.jsx
+++ front/src/Components/AccountRegister.jsx
@@ -2,19 +2,19 @@ import React from 'react'
 import './AccountRegister.css'
 
 const AccountRegister = () => {
-  return (
-    <form method='post' className='accountRegisterForm'>
-      <label htmlFor='email'>Adresse mail :</label>
-      <input type='email' name='email' id='email' required />
-      <label htmlFor='password'>Mot de passe :</label>
-      <input type='password' name='password' id='password' required />
-      <label htmlFor='password2'>Confirmation du mot de passe :</label>
-      <input type='password' name='password2' id='password2' />
-      <button type='submit' id='submit'>
-        S'inscrire
-      </button>
-    </form>
-  )
+    return (
+        <form method='post' className='accountRegisterForm'>
+            <label htmlFor='email'>Adresse mail :</label>
+            <input type='email' name='email' id='email' required />
+            <label htmlFor='password'>Mot de passe :</label>
+            <input type='password' name='password' id='password' required />
+            <label htmlFor='password2'>Confirmation du mot de passe :</label>
+            <input type='password' name='password2' id='password2' />
+            <button type='submit' id='submit'>
+                S'inscrire
+            </button>
+        </form>
+    )
 }
 
 export default AccountRegister
