e8d5d2e1f714c689f7469d7ca814f7d88c1bddf5
Pierre-Alban DEMARS <demarspierrealban@gmail.com>
2020-01-14 15:05:59 +0100

    Remake CSS of Navbar

--- front/src/Components/Navbar.jsx
+++ front/src/Components/Navbar.jsx
@@ -7,25 +7,28 @@ import {
     faScroll
 } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import './Navbar.css'
 import { Link } from 'react-router-dom'
+import './Navbar.css'
 
 library.add(faCommentDots, faTags, faUser, faScroll)
 
 const Navbar = () => {
     return (
-        <div className='Navbar'>
-            <Link to='' className='navlink'>
-                <FontAwesomeIcon icon='scroll' className='navIcon' />
+        <div className='flex-row fixed-bottom-1 width100 space-around border'>
+            <Link to='' className=''>
+                <FontAwesomeIcon icon='scroll' className='width25 icon-zone' />
             </Link>
-            <Link to='/tags' className='navlink'>
-                <FontAwesomeIcon icon='tags' className='navIcon' />
+            <Link to='/tags' className=''>
+                <FontAwesomeIcon icon='tags' className='width25 icon-zone' />
             </Link>
-            <Link to='/profiles' className='navlink'>
-                <FontAwesomeIcon icon='user' className='navIcon' />
+            <Link to='/profiles' className=''>
+                <FontAwesomeIcon icon='user' className='width25 icon-zone' />
             </Link>
-            <Link to='' className='navlink'>
-                <FontAwesomeIcon icon='comment-dots' className='navIcon' />
+            <Link to='' className=''>
+                <FontAwesomeIcon
+                    icon='comment-dots'
+                    className='width25 icon-zone'
+                />
             </Link>
         </div>
     )
