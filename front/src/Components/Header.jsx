12333fdb262754310f5d7684064da06c146e56ae
Pierre-Alban DEMARS <demarspierrealban@gmail.com>
2020-01-14 09:25:00 +0100

    ADD Header Component

---
+++ front/src/Components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+    return (
+        <div className='header'>
+            <h2>JaMusic</h2>
+        </div>
+    )
+}
+
+export default Header
