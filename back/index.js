91a3e03608ae0927561b4fdcefbc9545fbaeb5b5
Brolyus <desousanthony@gmail.com>
2020-01-20 11:36:08 +0100

    fixed the sql request on /profiles/:id route also

--- back/index.js
+++ back/index.js
@@ -146,14 +146,12 @@ app.route('/profiles/:id')
 		const param = request.query.token
 		const idProfile = request.params.id
 		jwt.verify(param, secret, (err, authData) => {
-			const userEmail = authData.iss
-			console.log(authData)
 			if (err) {
 				response.sendStatus(401)
 			} else {
 				connection.query(
 					//retrieve only id 1 because we are changing database, will be updated on next PR
-					`SELECT * FROM profile INNER JOIN account on email = '${userEmail}'`,
+					`SELECT * FROM profile WHERE id = '${idProfile}'`,
 					[idProfile],
 					(err, results) => {
 						if (err) {
