0f1ff1156cad74a241e79e39661f0617fd02d1b0
Brolyus <desousanthony@gmail.com>
2020-01-20 13:59:42 +0100

    merged everything for tests

--- front/src/Components/Profile.jsx
+++ front/src/Components/Profile.jsx
@@ -15,7 +15,7 @@ class Profile extends React.Component {
 		}
 	}
 	componentDidMount() {
-		url = `http://localhost:3000/profiles/${this.state.id}`
+		const url = `http://localhost:3000/profiles/${this.state.id}`
 		axios
 			.get(url, {
 				params: {
