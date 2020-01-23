3bcbdcd86218b4f78da2e1758c90e5148c3dd83e
Pierre-Alban DEMARS <demarspierrealban@gmail.com>
2020-01-23 09:46:43 +0100

    Add demo datas for posts

--- database/post.sql
+++ database/post.sql
@@ -1,18 +1,46 @@
-INSERT INTO `post` ( `id`, `text`, `media`, `likes`, `share`, `profile_id` ) VALUES ( 1, `Je suis un développeur de jeu indépendant et je recherche un artiste Synthwave pour la bande-son de mon jeu.`; `https://cdn.pixabay.com/photo/2019/01/19/13/12/synthwave-3941721_960_720.jpg`, 584, 762, 3 );
+INSERT INTO "post"
+    ( "id", "text", "media", "likes", "share", "profile_id" )
+VALUES
+    ( 1, "Je suis un développeur de jeu indépendant et je recherche un artiste Synthwave pour la bande-son de mon jeu.";
+"https://cdn.pixabay.com/photo/2019/01/19/13/12/synthwave-3941721_960_720.jpg", 584, 762, 3 );
 
-INSERT INTO `post`( `id`, `text`, `media`, `likes`, `share`, `profile_id` ) VALUES (4, `Je viens d écouter le premier morceau qui fera partie de la BO. Il est IN-CRO-YABLE !`, ``, 78, 58, 1);
+INSERT INTO "post"
+    ( "id", "text", "media", "likes", "share", "profile_id" )
+VALUES
+    (4, "Je viens d écouter le premier morceau qui fera partie de la BO. Il est IN-CRO-YABLE !", "", 78, 58, 1);
 
-INSERT INTO `post`( `id`, `text`, `media`, `likes`, `share`, `profile_id` ) VALUES (5, `Ca fait maintenant 3 semaines que j écoute BlackPink en boucle, c est vraiment exceptionnel ! Allez écouter celle-là !`, ``, 125, 96, 1);
+INSERT INTO "post"
+    ( "id", "text", "media", "likes", "share", "profile_id" )
+VALUES
+    (5, "Ca fait maintenant 3 semaines que j écoute BlackPink en boucle, c est vraiment exceptionnel ! Allez écouter celle-là !", "", 125, 96, 1);
 
-INSERT INTO `post`( `id`, `text`, `media`, `likes`, `share`, `profile_id` ) VALUES ( 2, `Salut,
-On cherche un guitariste pour un concert dans deux semaines, le notre est tombé malade.`, ``, 10, 2, 2 );
+INSERT INTO "post"
+    ( "id", "text", "media", "likes", "share", "profile_id" )
+VALUES
+    ( 2, "Salut,
+ON cherche un guitariste pour un concert dans deux semaines, le notre est tombé malade.", "", 10, 2, 2 );
 
-INSERT INTO `post`( `id`, `text`, `media`, `likes`, `share`, `profile_id` ) VALUES (6, `Je jouerai le week-end prochain dans un bar au centre-ville.`, ``, 74, 35, 2);
+INSERT INTO "post"
+    ( "id", "text", "media", "likes", "share", "profile_id" )
+VALUES
+    (6, "Je jouerai le week-end prochain dans un bar au centre-ville.", "", 74, 35, 2);
 
-INSERT INTO `post`( `id`, `text`, `media`, `likes`, `share`, `profile_id` ) VALUES (7, `L ambiance était folle !`, ``, 52, 36, 2);
+INSERT INTO "post"
+    ( "id", "text", "media", "likes", "share", "profile_id" )
+VALUES
+    (7, "L ambiance était folle !", "", 52, 36, 2);
 
-INSERT INTO `post`( `id`, `text`, `media`, `likes`, `share`, `profile_id` ) VALUES ( `3`, `Salut à tous, je suis bassiste depuis 5 ans et je suis à la recherche d un groupe de blues. Voici un exemple de mes compétences ->`, ``, 54, 19, 3 );
+INSERT INTO "post"
+    ( "id", "text", "media", "likes", "share", "profile_id" )
+VALUES
+    ( "3", "Salut à tous, je suis bassiste depuis 5 ans et je suis à la recherche d un groupe de blues. Voici un exemple de mes compétences ->", "", 54, 19, 3 );
 
-INSERT INTO `post`( `id`, `text`, `media`, `likes`, `share`, `profile_id` ) VALUES (8, `Voici, ma nouvelle composition allez écouter ça ->`, ``, 63, 24, 3);
+INSERT INTO "post"
+    ( "id", "text", "media", "likes", "share", "profile_id" )
+VALUES
+    (8, "Voici, ma nouvelle composition allez écouter ça ->", "", 63, 24, 3);
 
-INSERT INTO `post`( `id`, `text`, `media`, `likes`, `share`, `profile_id` ) VALUES (9, `Merci pour tous les retours :D`, ``, 87, 64, 3);
+INSERT INTO "post"
+    ( "id", "text", "media", "likes", "share", "profile_id" )
+VALUES
+    (9, "Merci pour tous les retours :D", "", 87, 64, 3);
