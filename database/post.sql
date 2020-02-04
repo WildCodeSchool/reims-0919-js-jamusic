1738a60b5a8df0ad6f6b6cb4451f896207379cb3
Brolyus <desousanthony@gmail.com>
2020-02-04 11:17:04 +0100

    added posts

--- database/post.sql
+++ database/post.sql
@@ -1,47 +1,126 @@
 USE jamusic;
-INSERT INTO post
-    ( id, text, media, likes, share, profile_id )
-VALUES
-    ( 1, "Je suis un développeur de jeu indépendant et je recherche un artiste Synthwave pour la bande-son de mon jeu.",
-        "https://cdn.pixabay.com/photo/2019/01/19/13/12/synthwave-3941721_960_720.jpg", 584, 762, 3 );
 
-INSERT INTO post
-    ( id, text, media, likes, share, profile_id )
+INSERT INTO
+    post (id, text, media, likes, share, profile_id)
 VALUES
-    (4, "Je viens d écouter le premier morceau qui fera partie de la BO. Il est IN-CRO-YABLE !", "", 78, 58, 1);
-
-INSERT INTO post
-    ( id, text, media, likes, share, profile_id )
-VALUES
-    (5, "Ca fait maintenant 3 semaines que j écoute BlackPink en boucle, c est vraiment exceptionnel ! Allez écouter celle-là !", "", 125, 96, 1);
-
-INSERT INTO post
-    ( id, text, media, likes, share, profile_id )
-VALUES
-    ( 2, "Salut,
-ON cherche un guitariste pour un concert dans deux semaines, le notre est tombé malade.", "", 10, 2, 2 );
-
-INSERT INTO post
-    ( id, text, media, likes, share, profile_id )
-VALUES
-    (6, "Je jouerai le week-end prochain dans un bar au centre-ville.", "", 74, 35, 2);
-
-INSERT INTO post
-    ( id, text, media, likes, share, profile_id )
-VALUES
-    (7, "L ambiance était folle !", "", 52, 36, 2);
-
-INSERT INTO post
-    ( id, text, media, likes, share, profile_id )
-VALUES
-    ( "3", "Salut à tous, je suis bassiste depuis 5 ans et je suis à la recherche d un groupe de blues. Voici un exemple de mes compétences ->", "", 54, 19, 3 );
-
-INSERT INTO post
-    ( id, text, media, likes, share, profile_id )
-VALUES
-    (8, "Voici, ma nouvelle composition allez écouter ça ->", "", 63, 24, 3);
-
-INSERT INTO post
-    ( id, text, media, likes, share, profile_id )
-VALUES
-    (9, "Merci pour tous les retours :D", "", 87, 64, 3);
+    (
+        1,
+        "Je suis un développeur de jeu indépendant et je recherche un artiste Synthwave pour la bande-son de mon jeu.",
+        "https://cdn.pixabay.com/photo/2019/01/19/13/12/synthwave-3941721_960_720.jpg",
+        584,
+        762,
+        3
+    ),
+    (
+        4,
+        "Je viens d écouter le premier morceau qui fera partie de la BO. Il est IN-CRO-YABLE !",
+        "",
+        78,
+        58,
+        1
+    ),
+    (
+        5,
+        "Ca fait maintenant 3 semaines que j écoute BlackPink en boucle, c est vraiment exceptionnel ! Allez écouter celle-là !",
+        "",
+        125,
+        96,
+        1
+    ),
+    (
+        2,
+        "Salut,
+ON cherche un batteur pour un concert dans deux semaines, le notre est tombé malade.",
+        "",
+        10,
+        2,
+        2
+    ),
+    (
+        6,
+        "Je jouerai le week-end prochain dans un bar au centre-ville.",
+        "",
+        74,
+        35,
+        2
+    ),
+    (
+        7,
+        "L ambiance était folle !",
+        "",
+        52,
+        36,
+        2
+    ),
+    (
+        3,
+        "Salut à tous, je suis bassiste depuis 5 ans et je suis à la recherche d un groupe de blues. Voici un exemple de mes compétences ->",
+        "",
+        54,
+        19,
+        3
+    ),
+    (
+        8,
+        "Voici, ma nouvelle composition allez écouter ça ->",
+        "",
+        63,
+        24,
+        3
+    ),
+    (
+        9,
+        "Merci pour tous les retours :D",
+        "",
+        87,
+        64,
+        3
+    ),
+    (
+        10,
+        "L'ambiance était top, merci à tous d'être venus au concert",
+        "https://ordo-ab-chao.fr/wp-content/uploads/2018/02/Codex-Magica-3-Messages-occultes-et-signes-de-la-main.jpg",
+        100,
+        321,
+        17
+    ),
+    (
+        11,
+        "Bonne continuation Jules, ça va pas être facile de trouver un guitariste aussi doué pour la suite",
+        "https://i.ytimg.com/vi/8II4oKMvudk/hqdefault.jpg",
+        66,
+        122,
+        17
+    ),
+    (
+        12,
+        "Yes, une nouvelle date pour un concert à La Petite Halle, on vous attend nombreux le 14/2 en première partie de Frankie Vincent",
+        "https://quartierlibre.co/sites/default/files/styles/1200/public/upload/COUV_FB_ST_VALENTIN.jpg?itok=KXGhQ6bN",
+        55,
+        19,
+        17
+    ),
+    (
+        13,
+        "J'ai été au dernier concert de Retroid Mania, vraiment une super soirée",
+        "",
+        129,
+        10,
+        10
+    ),
+    (
+        14,
+        "Je donne des cours de piano, envoyez moi un mail si vous êtes intéressés",
+        "https://media.istockphoto.com/photos/close-up-of-happy-womans-hand-playing-the-piano-in-the-morning-picture-id901538534?k=6&m=901538534&s=612x612&w=0&h=QnA36AI-c6BILne6DP5Av8vIZXsiNPexpkl8FKzbMXk=",
+        222,
+        170,
+        10
+    ),
+    (
+        15,
+        "On organise un boeuf le 4/2 à la petite halle, des gens intéressés ?",
+        "",
+        30,
+        19,
+        7
+    );
